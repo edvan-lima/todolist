20c2b79a319d18132631858a2f29d9f37d69f98b
Edvan Lima <edvan.lima@tripletech.com.br>
2023-10-13 21:23:09 -0300

    adicionado o projeto no git

---
+++ src/test/java/br/com/edvan/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.edvan.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
